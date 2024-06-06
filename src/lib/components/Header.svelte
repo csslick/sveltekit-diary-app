73ddadc6931356f63a4ecd724ea24f53e4592bfe
csslick <digitone@naver.com>
2024-06-06 19:21:04 +0900

    77.Header 영역 디자인

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,32 @@
 <header>
-  <nav>
-    <a href="/">home</a>
-    <a href="/read">read</a>
-    <a href="/write">write</a>
-    <a href="/edit">edit</a>
-  </nav>
+  <h1>Diary</h1>
+  <button class="btn">완료</button>
 </header>
+<nav>
+  <a href="/">home</a>
+  <a href="/read">read</a>
+  <a href="/write">write</a>
+  <a href="/edit">edit</a>
+</nav>
 
 <style lang="scss">
   header {
-    background: #ccc;
-    a {
-      text-decoration: none;
-      color: #333;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    background: var(--main-color);
+    color: var(--text-light);
+    // a {
+    //   text-decoration: none;
+    //   color: var(--text-light);
+    // }
+    h1 { font-size: 28px; }
+    .btn {
+      border: none;
+      background: transparent;
+      color: var(--text-light);
+      font-size: 18px;
+      cursor: pointer;
     }
   }
 </style>
