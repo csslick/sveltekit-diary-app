b6f09d2cd0c042366f8940bc13f3d87a701ef5a0
csslick <digitone@naver.com>
2024-06-09 16:34:45 +0900

    86.글쓰기 -  입력 기능 추가하기

--- src/routes/write/+page.svelte
+++ src/routes/write/+page.svelte
@@ -1,1 +1,30 @@
-<h1>Write</h1>
+<script>
+  const formatDate = function() {
+    return new Date().toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+    });
+  }
+</script>
+
+<main>
+  <div class="diary">
+    <textarea placeholder="글쓰기를 시작하세요..."></textarea>
+    <div class="bottom-info">
+      <span class="date">{formatDate()}</span>
+    </div>
+  </div>
+</main>
+
+<style lang='scss'>
+  textarea {
+    border: none;
+    width: 100%;
+    font-size: inherit;
+    padding: 0.5rem 0;
+    background: transparent;
+    outline: none;
+  }
+</style>
