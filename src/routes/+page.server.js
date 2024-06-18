d82cd317173333ca44fdb735a5480cdafc12d37b
csslick <digitone@naver.com>
2024-06-18 14:07:14 +0900

    102.글 목록 최신순으로 정렬하기

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,11 @@
 import { supabase } from "../lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("diaries").select();
+  const { data } = 
+    await supabase
+      .from("diaries")
+      .select()
+      .order('id', { ascending: false });
 
   return {
     diaries: data
